792d9032e493f3fc8c3ab183015bff6be143f2e6
Adria <adriafigueresgarcia@gmail.com>
2020-09-30 08:07:12 +0200

    functions BD

--- bd/bd.php
+++ bd/bd.php
@@ -20,4 +20,59 @@ function desconectar(){
     $GLOBALS['pdo'] = null;
 }
 
+function metodoGet($query){
+    try {
+        conectar();
+        $statement = $GLOBALS['pdo']->prepare($query);
+        $statement = setFetchMode(PDO::FETCH_ASSOC);
+        $statement->execute();
+        desconectar();
+        return $statement;
+    } catch (Exception $e) {
+        die("Error: ". $e);
+    }
+}
+
+function metodoPost($query, $queryAutoIncrement){
+    try {
+        conectar();
+        $statement = $GLOBALS['pdo']->prepare($query);
+        $statement->execute();
+        $idAutoIncrement= metodoGet($queryAutoIncrement)->fetch(PDO::FECTH_ASSOC);
+        $result = array_merge($idAutoIncrement, $_POST);
+        $statement->closeCursor();
+        desconectar();
+        return $result;
+    } catch (Exception $e) {
+        die("Error: ". $e);
+    }
+}
+
+function metodoPut($query){
+    try {
+        conectar();
+        $statement = $GLOBALS['pdo']->prepare($query);
+        $statement->execute();
+        $result = array_merge($_GET, $_POST);
+        $statement->closeCursor();
+        desconectar();
+        return $result;
+    } catch (Exception $e) {
+        die("Error: ". $e);
+    }
+}
+
+function metodoDelete($query){
+    try {
+        conectar();
+        $statement = $GLOBALS['pdo']->prepare($query);
+        $statement->execute();
+        $statement->closeCursor();
+        desconectar();
+        return $_GET['id'];
+    } catch (Exception $e) {
+        die("Error: ". $e);
+    }
+}
+
 ?>
